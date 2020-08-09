aa327ed9c98abd5a1907cf3028d6472798dd4d4a
Owanesh <svt.busiello@gmail.com>
2020-08-09 16:03:09 +0200

    feat(validator): add regexp to validate url before using it

--- src/shortnim.nim
+++ src/shortnim.nim
@@ -1,7 +1,6 @@
 let doc = """
 Shortnim ✂️ 🔗                                
 
-
 Usage:
   shortnim relink <url>
   shortnim cleanuri <url>
@@ -13,24 +12,26 @@ Options:
   -h --help     Show this screen.
   -v --version  Show version.
 """
-
+import nre, regex
 import strutils, sequtils
 import docopt
 import providers/[cleanuriProvider, relinkProvider]
 
 let args = docopt(doc, version = "shortnim 0.1a")
 let supported_provider = ["relink","cleanuri"]
+var url_pattern = re"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$"
 
- 
-
-if args["--provider"] and ($args["--provider"] in supported_provider):
+if args["--provider"] and ($args["--provider"] in supported_provider) and match($args["<url>"], url_pattern):
   case $args["--provider"]:
     of "relink":
       relink($args["<url>"])
     of "cleanuri":
       cleanuri($args["<url>"])
-
+elif args["<url>"] and not match($args["<url>"], url_pattern) :
+  echo "Sorry, your url ", $args["<url>"], " does not appear correct for shortnim"
 elif args["--provider"] and ($args["--provider"] notin supported_provider):
   echo "Sorry, provider ", $args["--provider"], " isn't supported yet"
+else:
+  echo doc
 
 
